d4aa3fddce458dcb30e7afd08c81556d980dcf50
Your Name <you.example.com>
2014-06-27 18:57:52 +0000

    Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def home
+  end
+  
+  def show
+    
+    @movie = Movie.find_by_id(params['id'])
+    
+  end
 end
