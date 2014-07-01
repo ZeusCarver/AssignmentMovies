813444752059effa28a8dfcd41853e9ba4269f66
Your Name <you.example.com>
2014-07-01 14:25:02 +0000

    Now with adding movies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,18 @@ class ApplicationController < ActionController::Base
   def home
   end
   
+  def new
+  end
+  
+  def store
+    m = Movie.new
+    m.name = params['name']
+    m.image = params['image']
+    m.link = params['link']
+    m.save
+    redirect_to '/'
+  end
+  
   def show
     
     @movie = Movie.find_by_id(params['id'])
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get '/' => 'application#home'
+  get '/new' => 'application#new'
+  get '/store' => 'application#store'
   get '/:id' => 'application#show'
+  
+  
 end
